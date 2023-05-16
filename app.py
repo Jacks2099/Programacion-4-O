b7b718bc0c3513b7c5466fb6a38d20157e24a801
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-15 19:31:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ if button1:
     '¿Cuantos volumenes requieres?',
     ('5', '6', '7', '8', '9', '10'))
   if '5':
-    a= st.input("Ingresa volumen 1: ")
-    b= st.input("Ingresa volumen 2: ")
-    c= st.input("Ingresa volumen 3: ")
-    d= st.input("Ingresa volumen 4: ")
-    e= st.input("Ingresa volumen 5: ")
+    a= st.str(input("Ingresa volumen 1: "))
+    b= st.str(input("Ingresa volumen 2: "))
+    c= st.str(input("Ingresa volumen 3: "))
+    d= st.str(input("Ingresa volumen 4: "))
+    e= st.str(input("Ingresa volumen 5: "))
     
     v= 0.1
     w= 0.01
@@ -41,6 +41,64 @@ if button1:
   st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
 
 elif button2:
-  st.write(':poop:')
+  option = st.selectbox(
+    '¿Cuantos volumenes requieres?',
+    ('5', '6', '7', '8', '9', '10'))
+  if '5':
+    a= st.str(input("Ingresa volumen 1: "))
+    b= st.str(input("Ingresa volumen 2: "))
+    c= st.str(input("Ingresa volumen 3: "))
+    d= st.str(input("Ingresa volumen 4: "))
+    e= st.str(input("Ingresa volumen 5: "))
+    
+    v= 0.1
+    w= 0.01
+    x= 0.001
+    y= 0.0001
+    z= 0.00001
+    
+    ph1= np.log10(v)*-1
+    ph2= np.log10(w)*-1
+    ph3= np.log10(x)*-1
+    ph4= np.log10(y)*-1
+    ph5= np.log10(z)*-1
+    
+    datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
+    datos
+    
+    st.write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
+    st.write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
+    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
+    
+  st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
 elif button3:
-  st.write(':dog:')
+  option = st.selectbox(
+    '¿Cuantos volumenes requieres?',
+    ('5', '6', '7', '8', '9', '10'))
+  if '5':
+    a= st.str(input("Ingresa volumen 1: "))
+    b= st.str(input("Ingresa volumen 2: "))
+    c= st.str(input("Ingresa volumen 3: "))
+    d= st.str(input("Ingresa volumen 4: "))
+    e= st.str(input("Ingresa volumen 5: "))
+    
+    v= 0.1
+    w= 0.01
+    x= 0.001
+    y= 0.0001
+    z= 0.00001
+    
+    ph1= np.log10(v)*-1
+    ph2= np.log10(w)*-1
+    ph3= np.log10(x)*-1
+    ph4= np.log10(y)*-1
+    ph5= np.log10(z)*-1
+    
+    datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
+    datos
+    
+    st.write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
+    st.write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
+    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
+    
+  st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
