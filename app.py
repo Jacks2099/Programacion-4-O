6e4b9a6342c1511491a7a049b9c9cd93227e4d9b
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-15 19:12:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.button_1('Acido-Base Fuerte')
-st.button_2('Acido Fuerte-Base Debil')
-st.button_3('Acido Debil-Base Fuerte')
+button1 = st.button('Acido-Base Fuerte')
+button2 = st.button('Acido Fuerte-Base Debil')
+button3 = st.button('Acido Debil-Base Fuerte')
 
-if button_1:
+if button1:
   x=float(input(""))
   y=float(input(""))
   z=float(input(""))
@@ -18,8 +18,13 @@ if button_1:
   datos=pd.DataFrame({'Volumen': [1, 2, 3], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2)]}, index=["Lab 1", "Lab 2", "Lab 3"])
   datos
 
-  write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
-  write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
-  write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
+  st.write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
+  st.write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
+  st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
 
   st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
+
+elif button2:
+  st.write(':poop:')
+elif button3:
+  st.write(':dog:')
