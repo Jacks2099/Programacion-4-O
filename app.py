13fdbcf9e56bbdffd51861cce415291fcf6f0654
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-15 20:03:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import numpy as np
 
 st.title('_Curvas de Titulación_')
 
-nested_btn = button1 = st.button('Acido-Base Fuerte')
-nested_btn = button2 = st.button('Acido Fuerte-Base Debil')
-nested_btn = button3 = st.button('Acido Debil-Base Fuerte')
+button1 = st.button('Acido-Base Fuerte')
+button2 = st.button('Acido Fuerte-Base Debil')
+button3 = st.button('Acido Debil-Base Fuerte')
 
 if button1:
   option = st.selectbox(
@@ -50,20 +50,23 @@ if button1:
     c= st.text_input("Ingresa volumen 3:", value=" ")
     d= st.text_input("Ingresa volumen 4:", value=" ")
     e= st.text_input("Ingresa volumen 5:", value=" ")
+    f= st.text_input("Ingresa volumen 6:", value=" ")
     
-    v= 0.1
-    w= 0.01
-    x= 0.001
-    y= 0.0001
-    z= 0.00001
+    u= 0.1
+    v= 0.01
+    w= 0.001
+    x= 0.0001
+    y= 0.00001
+    z= 0.000001
     
-    ph1= np.log10(v)*-1
-    ph2= np.log10(w)*-1
-    ph3= np.log10(x)*-1
-    ph4= np.log10(y)*-1
-    ph5= np.log10(z)*-1
+    ph1= np.log10(u)*-1
+    ph2= np.log10(v)*-1
+    ph3= np.log10(w)*-1
+    ph4= np.log10(x)*-1
+    ph5= np.log10(y)*-1
+    ph6= np.log10(z)*-1
     
-    datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
+    datos=pd.DataFrame({'Volumen': [a, b, c, d, e, f], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2), np.round(ph6, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5", "Lab 6"])
     datos
     
     st.header('Cálculos')
@@ -73,76 +76,11 @@ if button1:
     st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
     st.write("pH=-log10("+str(z)+")= "+str(np.round(ph4, 2)))
     st.write("pH=-log10("+str(z)+")= "+str(np.round(ph5, 2)))
+    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph6, 2)))
     
     st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
 
 elif button2:
-  option = st.selectbox(
-    '¿Cuantos volumenes requieres?',
-    ('5', '6', '7', '8', '9', '10'))
-  if '5':
-    a= st.text_input("Ingresa volumen 1:", value=" ")
-    b= st.text_input("Ingresa volumen 2:", value=" ")
-    c= st.text_input("Ingresa volumen 3:", value=" ")
-    d= st.text_input("Ingresa volumen 4:", value=" ")
-    e= st.text_input("Ingresa volumen 5:", value=" ")
-    
-    v= 0.1
-    w= 0.01
-    x= 0.001
-    y= 0.0001
-    z= 0.00001
-    
-    ph1= np.log10(v)*-1
-    ph2= np.log10(w)*-1
-    ph3= np.log10(x)*-1
-    ph4= np.log10(y)*-1
-    ph5= np.log10(z)*-1
-    
-    datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
-    datos
-    
-    st.header('Cálculos')
-    
-    st.write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
-    st.write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
-    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
-    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph4, 2)))
-    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph5, 2)))
-    
-  st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
+  st.write('En proceso:hammer:')
 elif button3:
-  option = st.selectbox(
-    '¿Cuantos volumenes requieres?',
-    ('5', '6', '7', '8', '9', '10'))
-  if '5':
-    a= st.text_input("Ingresa volumen 1:", value=" ")
-    b= st.text_input("Ingresa volumen 2:", value=" ")
-    c= st.text_input("Ingresa volumen 3:", value=" ")
-    d= st.text_input("Ingresa volumen 4:", value=" ")
-    e= st.text_input("Ingresa volumen 5:", value=" ")
-    
-    v= 0.1
-    w= 0.01
-    x= 0.001
-    y= 0.0001
-    z= 0.00001
-    
-    ph1= np.log10(v)*-1
-    ph2= np.log10(w)*-1
-    ph3= np.log10(x)*-1
-    ph4= np.log10(y)*-1
-    ph5= np.log10(z)*-1
-    
-    datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
-    datos
-    
-    st.header('Cálculos')
-    
-    st.write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
-    st.write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
-    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
-    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph4, 2)))
-    st.write("pH=-log10("+str(z)+")= "+str(np.round(ph5, 2)))
-    
-  st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
+  st.write('En proceso:hammer:')
