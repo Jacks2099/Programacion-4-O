f76d5d283a841ed61a1ebf5a051dfd236170c271
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-16 20:55:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,28 +19,28 @@ if option == 'Acido-Base Fuerte':
       d= st.text_input("Ingresa volumen 4:", value=" ")
       e= st.text_input("Ingresa volumen 5:", value=" ")
       
-      v= 0.03333
-      w= 5.5555e-3
-      x= 1e-7
-      y= 4.5454e-3
-      z= 0.01666
+      C1= 0.03333
+      C2= 5.5555e-3
+      C3= 1e-7
+      C4= 4.5454e-3
+      C5= 0.01666
       
-      ph1= 14+np.log10(v)
-      ph2= 14+np.log10(w)
-      ph3= np.log10(x)*-1
-      ph4= np.log10(y)*-1
-      ph5= np.log10(z)*-1
+      ph1= 14+np.log10(C1)
+      ph2= 14+np.log10(C2)
+      ph3= np.log10(C3)*-1
+      ph4= np.log10(C4)*-1
+      ph5= np.log10(C5)*-1
       
       datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
       datos
       
       st.header('Cálculos')
       
-      st.write("pH=14+log10("+str(x)+")= "+str(np.round(ph1, 2)))
-      st.write("pH=14+log10("+str(y)+")= "+str(np.round(ph2, 2)))
-      st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
-      st.write("pH=-log10("+str(z)+")= "+str(np.round(ph4, 2)))
-      st.write("pH=-log10("+str(z)+")= "+str(np.round(ph5, 2)))
+      st.write("pH=14+log10("+str(C1)+")= "+str(np.round(ph1, 2)))
+      st.write("pH=14+log10("+str(C2)+")= "+str(np.round(ph2, 2)))
+      st.write("pH=-log10("+str(C3)+")= "+str(np.round(ph3, 2)))
+      st.write("pH=-log10("+str(C4)+")= "+str(np.round(ph4, 2)))
+      st.write("pH=-log10("+str(C5)+")= "+str(np.round(ph5, 2)))
       
       chart_data = pd.DataFrame({'volume':[a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]})
       st.line_chart(chart_data, x='volume', y='pH')
@@ -53,33 +53,33 @@ if option == 'Acido-Base Fuerte':
       e= st.text_input("Ingresa volumen 5:", value=" ")
       f= st.text_input("Ingresa volumen 6:", value=" ")
       
-      u= 0.1
-      v= 0.01
-      w= 0.001
-      x= 0.0001
-      y= 0.00001
-      z= 0.000001
+      C1= 0.05
+      C2= 0.02143
+      C3= 5.5555e-3
+      C4= 4.5454e-3
+      C5= 0.01154
+      C6= 0.01666
       
-      ph1= np.log10(u)*-1
-      ph2= np.log10(v)*-1
-      ph3= np.log10(w)*-1
-      ph4= np.log10(x)*-1
-      ph5= np.log10(y)*-1
-      ph6= np.log10(z)*-1
+      ph1= 14+np.log10(C1)
+      ph2= 14+np.log10(C2)
+      ph3= 14+np.log10(C3)
+      ph4= np.log10(C4)*-1
+      ph5= np.log10(C5)*-1
+      ph6= np.log10(C6)*-1
       
       datos=pd.DataFrame({'Volumen': [a, b, c, d, e, f], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2), np.round(ph6, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5", "Lab 6"])
       datos
       
       st.header('Cálculos')
       
-      st.write("pH=-log10("+str(x)+")= "+str(np.round(ph1, 2)))
-      st.write("pH=-log10("+str(y)+")= "+str(np.round(ph2, 2)))
-      st.write("pH=-log10("+str(z)+")= "+str(np.round(ph3, 2)))
+      st.write("pH=14+log10("+str(x)+")= "+str(np.round(ph1, 2)))
+      st.write("pH=14+log10("+str(y)+")= "+str(np.round(ph2, 2)))
+      st.write("pH=14+log10("+str(z)+")= "+str(np.round(ph3, 2)))
       st.write("pH=-log10("+str(z)+")= "+str(np.round(ph4, 2)))
       st.write("pH=-log10("+str(z)+")= "+str(np.round(ph5, 2)))
       st.write("pH=-log10("+str(z)+")= "+str(np.round(ph6, 2)))
       
-      st.line_chart(data=pd.DataFrame, x='Volumen', y='pH')
+      st.line_chart(datos, x='Volumen', y='pH')
 
 elif option == 'Acido Fuerte-Base Debil':
   st.write('En proceso:hammer:')
