47705eb6e6069e335e00faa732ef7f01add93638
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-16 21:22:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ option = st.selectbox(
   'Selecciona el tipo de curva',
   ('Acido-Base Fuerte', 'Acido Fuerte-Base Debil', 'Acido Debil-Base Fuerte'))
 
+CA = st.text_input("Ingresa la concentración del _analito_:", value=" ")
+VA = st.text_input("Ingresa volumen del _analito_:", value=" ")
+CT = st.text_input("Ingresa la concentración del _titulante_:", value=" ")
+mmolA = CA*VA
+
 if option == 'Acido-Base Fuerte':
   volumen = st.radio(
     '¿Cuantos volumenes requieres?',
@@ -17,108 +22,46 @@ if option == 'Acido-Base Fuerte':
       b= st.text_input("Ingresa volumen 2:", value=" ")
       c= st.text_input("Ingresa volumen 3:", value=" ")
       d= st.text_input("Ingresa volumen 4:", value=" ")
-      e= st.text_input("Ingresa volumen 5:", value=" ")
+
+      mmolT1 = CT*a
+      mmolT2 = CT*b
+      mmolT3 = CT*c
+      mmolT4 = CT*d
       
-      C1= 0.05
-      C2= 0.0125
-      C3= 1e-7
-      C4= 8.33e-3
-      C5= 0.01429
+      C1= (mmolA-mmolT1)/(VA+a)
+      C2= (mmolA-mmolT2)/(VA+b)
+      C3= (mmolA-mmolT3)/(VA+c)
+      C4= (mmolA-mmolT4)/(VA+d)
       
-      ph1= 14+np.log10(C1)
-      ph2= 14+np.log10(C2)
-      ph3= np.log10(C3)*-1
-      ph4= np.log10(C4)*-1
-      ph5= np.log10(C5)*-1
+      ph1= np.log10(CA)*-1
+      ph2= np.log10(C1)*-1
+      ph3= np.log10(C2)*-1
+      ph4= np.log10(C3)*-1
+      ph5= np.log10(C4)*-1
       
-      datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
+      datos=pd.DataFrame({'Volumen': [0, a, b, c, d], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
       datos
       
       st.header('Cálculos')
       
-      st.write("pH=14+log10("+str(C1)+")= "+str(np.round(ph1, 2)))
-      st.write("pH=14+log10("+str(C2)+")= "+str(np.round(ph2, 2)))
-      st.write("pH=-log10("+str(C3)+")= "+str(np.round(ph3, 2)))
-      st.write("pH=-log10("+str(C4)+")= "+str(np.round(ph4, 2)))
-      st.write("pH=-log10("+str(C5)+")= "+str(np.round(ph5, 2)))
+      st.write("pH=-log10("+str(CA)+")= "+str(np.round(ph1, 2)))
+      st.write("pH=-log10("+str(C1)+")= "+str(np.round(ph2, 2)))
+      st.write("pH=-log10("+str(C2)+")= "+str(np.round(ph3, 2)))
+      st.write("pH=-log10("+str(C3)+")= "+str(np.round(ph4, 2)))
+      st.write("pH=-log10("+str(C4)+")= "+str(np.round(ph5, 2)))
       
       st.line_chart(datos, x='Volumen', y='pH')
       
   elif volumen == '6':
-      a= st.text_input("Ingresa volumen 1:", value=" ")
-      b= st.text_input("Ingresa volumen 2:", value=" ")
-      c= st.text_input("Ingresa volumen 3:", value=" ")
-      d= st.text_input("Ingresa volumen 4:", value=" ")
-      e= st.text_input("Ingresa volumen 5:", value=" ")
-      f= st.text_input("Ingresa volumen 6:", value=" ")
-      
-      C1= 0.05
-      C2= 0.02143
-      C3= 5.5555e-3
-      C4= 4.5454e-3
-      C5= 0.01154
-      C6= 0.01429
-      
-      ph1= 14+np.log10(C1)
-      ph2= 14+np.log10(C2)
-      ph3= 14+np.log10(C3)
-      ph4= np.log10(C4)*-1
-      ph5= np.log10(C5)*-1
-      ph6= np.log10(C6)*-1
-      
-      datos=pd.DataFrame({'Volumen': [a, b, c, d, e, f], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2), np.round(ph6, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5", "Lab 6"])
-      datos
-      
-      st.header('Cálculos')
-      
-      st.write("pH=14+log10("+str(C1)+")= "+str(np.round(ph1, 2)))
-      st.write("pH=14+log10("+str(C2)+")= "+str(np.round(ph2, 2)))
-      st.write("pH=14+log10("+str(C3)+")= "+str(np.round(ph3, 2)))
-      st.write("pH=-log10("+str(C4)+")= "+str(np.round(ph4, 2)))
-      st.write("pH=-log10("+str(C5)+")= "+str(np.round(ph5, 2)))
-      st.write("pH=-log10("+str(C6)+")= "+str(np.round(ph6, 2)))
-      
-      st.line_chart(datos, x='Volumen', y='pH')
-      
+      st.write('En proceso:hammer:')
   elif volumen == '7':
-      a= st.text_input("Ingresa volumen 1:", value=" ")
-      b= st.text_input("Ingresa volumen 2:", value=" ")
-      c= st.text_input("Ingresa volumen 3:", value=" ")
-      d= st.text_input("Ingresa volumen 4:", value=" ")
-      e= st.text_input("Ingresa volumen 5:", value=" ")
-      f= st.text_input("Ingresa volumen 6:", value=" ")
-      g= st.text_input("Ingresa volumen 7:", value=" ")
-      
-      C1= 0.05
-      C2= 0.02143
-      C3= 5.5555e-3
-      C4= 1e-7
-      C5= 4.5454e-3
-      C6= 0.01154
-      C7= 0.01429
-      
-      ph1= 14+np.log10(C1)
-      ph2= 14+np.log10(C2)
-      ph3= 14+np.log10(C3)
-      ph4= np.log10(C4)*-1
-      ph5= np.log10(C5)*-1
-      ph6= np.log10(C6)*-1
-      ph7= np.log10(C7)*-1
-      
-      datos=pd.DataFrame({'Volumen': [a, b, c, d, e, f, g], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2), np.round(ph6, 2), np.round(ph7, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5", "Lab 6", "Lab 7"])
-      datos
-      
-      st.header('Cálculos')
-      
-      st.write("pH=14+log10("+str(C1)+")= "+str(np.round(ph1, 2)))
-      st.write("pH=14+log10("+str(C2)+")= "+str(np.round(ph2, 2)))
-      st.write("pH=14+log10("+str(C3)+")= "+str(np.round(ph3, 2)))
-      st.write("pH=-log10("+str(C4)+")= "+str(np.round(ph4, 2)))
-      st.write("pH=-log10("+str(C5)+")= "+str(np.round(ph5, 2)))
-      st.write("pH=-log10("+str(C6)+")= "+str(np.round(ph6, 2)))
-      st.write("pH=-log10("+str(C7)+")= "+str(np.round(ph7, 2)))
-      
-      st.line_chart(datos, x='Volumen', y='pH')
+      st.write('En proceso:hammer:')
+  elif volumen == '8':
+    st.write('En proceso:hammer:')
+  elif volumen == '9':
+    st.write('En proceso:hammer:')
+  elif volumen == '10':
+    st.write('En proceso:hammer:')
 elif option == 'Acido Fuerte-Base Debil':
   st.write('En proceso:hammer:')
 elif option == 'Acido Debil-Base Fuerte':
