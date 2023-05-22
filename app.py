b835ee4c66382839b770c26118ab87794adaeb26
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-22 15:11:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ if option == 'Acido-Base (Fuertes)':
         C7= (float(mT7)-float(mA))/(float(VA)+float(g))
         ph7= 14+np.log10(float(C7))
       mT8 = np.multiply(float(CT), float(h))
-      if mT7<mA:
+      if mT8<mA:
         C8= (float(mA)-float(mT8))/(float(VA)+float(h))
         ph8= np.log10(float(C8))*-1
       elif mT8==mA:
@@ -314,10 +314,125 @@ if option == 'Acido-Base (Fuertes)':
       st.line_chart(datos, x=[a, b, c, d, e, f, g, h], y='pH')
       
   elif volumen == '9':
-    st.write('En proceso:hammer:')
+      a= st.text_input("Ingresa volumen 1:", value=" ")
+      b= st.text_input("Ingresa volumen 2:", value=" ")
+      c= st.text_input("Ingresa volumen 3:", value=" ")
+      d= st.text_input("Ingresa volumen 4:", value=" ")
+      e= st.text_input("Ingresa volumen 5:", value=" ")
+      f= st.text_input("Ingresa volumen 6:", value=" ")
+      g= st.text_input("Ingresa volumen 7:", value=" ")
+      h= st.text_input("Ingresa volumen 8:", value=" ")
+      i= st.text_input("Ingresa volumen 9:", value=" ")
+      
+      mA = np.multiply(float(CA), float(VA))
+      mT1 = np.multiply(float(CT), float(a))
+      if mT1<mA:
+        C1= (float(mA)-float(mT1))/(float(VA)+float(a))
+        ph1= np.log10(float(C1))*-1
+      elif mT1==mA:
+        ph1=7
+      elif mT1>mA:
+        C1= (float(mT1)-float(mA))/(float(VA)+float(a))
+        ph1= 14+np.log10(float(C1))
+      mT2 = np.multiply(float(CT), float(b))
+      if mT2<mA:
+        C2= (float(mA)-float(mT2))/(float(VA)+float(b))
+        ph2= np.log10(float(C2))*-1
+      elif mT2==mA:
+        ph2=7
+      elif mT2>mA:
+        C2= (float(mT2)-float(mA))/(float(VA)+float(b))
+        ph2= 14+np.log10(float(C2))
+      mT3 = np.multiply(float(CT), float(c))
+      if mT3<mA:
+        C3= (float(mA)-float(mT3))/(float(VA)+float(c))
+        ph3= np.log10(float(C3))*-1
+      elif mT3==mA:
+        ph3=7
+      elif mT3>mA:
+        C3= (float(mT3)-float(mA))/(float(VA)+float(c))
+        ph3= 14+np.log10(float(C3))
+      mT4 = np.multiply(float(CT), float(d))
+      if mT4<mA:
+        C4= (float(mA)-float(mT4))/(float(VA)+float(d))
+        ph4= np.log10(float(C4))*-1
+      elif mT4==mA:
+        ph4=7
+      elif mT4>mA:
+        C4= (float(mT4)-float(mA))/(float(VA)+float(d))
+        ph4= 14+np.log10(float(C4))
+      mT5 = np.multiply(float(CT), float(e))
+      if mT5<mA:
+        C5= (float(mA)-float(mT5))/(float(VA)+float(e))
+        ph5= np.log10(float(C5))*-1
+      elif mT5==mA:
+        ph5=7
+      elif mT5>mA:
+        C5= (float(mT5)-float(mA))/(float(VA)+float(e))
+        ph5= 14+np.log10(float(C5))
+      mT6 = np.multiply(float(CT), float(f))
+      if mT6<mA:
+        C6= (float(mA)-float(mT6))/(float(VA)+float(f))
+        ph6= np.log10(float(C6))*-1
+      elif mT6==mA:
+        ph6=7
+      elif mT6>mA:
+        C6= (float(mT6)-float(mA))/(float(VA)+float(f))
+        ph6= 14+np.log10(float(C6))
+      mT7 = np.multiply(float(CT), float(g))
+      if mT7<mA:
+        C7= (float(mA)-float(mT7))/(float(VA)+float(g))
+        ph7= np.log10(float(C7))*-1
+      elif mT7==mA:
+        ph7=7
+      elif mT7>mA:
+        C7= (float(mT7)-float(mA))/(float(VA)+float(g))
+        ph7= 14+np.log10(float(C7))
+      mT8 = np.multiply(float(CT), float(h))
+      if mT8<mA:
+        C8= (float(mA)-float(mT8))/(float(VA)+float(h))
+        ph8= np.log10(float(C8))*-1
+      elif mT8==mA:
+        ph8=7
+      elif mT8>mA:
+        C8= (float(mT8)-float(mA))/(float(VA)+float(h))
+        ph8= 14+np.log10(float(C8))
+      mT9 = np.multiply(float(CT), float(i))
+      if mT9<mA:
+        C9= (float(mA)-float(mT9))/(float(VA)+float(i))
+        ph9= np.log10(float(C9))*-1
+      elif mT9==mA:
+        ph9=7
+      elif mT9>mA:
+        C9= (float(mT9)-float(mA))/(float(VA)+float(i))
+        ph9= 14+np.log10(float(C9))
+      
+      datos=pd.DataFrame({'Volumen': [a, b, c, d, e, f, g, h, i], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2), np.round(ph6, 2), np.round(ph7, 2), np.round(ph8, 2), np.round(ph9, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5", "Lab 6", "Lab 7", "Lab 8", "Lab 9"])
+      datos
+      
+      st.line_chart(datos, x=[a, b, c, d, e, f, g, h, i], y='pH')
+      
   elif volumen == '10':
     st.write('En proceso:hammer:')
 elif option == 'Base-Acido (Fuertes)':
   st.write('En proceso:hammer:')
 elif option == 'Experimental':
   ph1=st.text_input('Ingresa el primer pH:', value=' ')
+  ph2=st.text_input('Ingresa el segundo pH:', value=' ')
+  ph3=st.text_input('Ingresa el tercer pH:', value=' ')
+  ph4=st.text_input('Ingresa el cuarto pH:', value=' ')
+  ph5=st.text_input('Ingresa el quinto pH:', value=' ')
+  ph6=st.text_input('Ingresa el sexto pH:', value=' ')
+  ph7=st.text_input('Ingresa el septimo pH:', value=' ')
+
+  a= st.text_input("Ingresa volumen 1:", value=" ")
+  b= st.text_input("Ingresa volumen 2:", value=" ")
+  c= st.text_input("Ingresa volumen 3:", value=" ")
+  d= st.text_input("Ingresa volumen 4:", value=" ")
+  e= st.text_input("Ingresa volumen 5:", value=" ")
+  f= st.text_input("Ingresa volumen 6:", value=" ")
+  g= st.text_input("Ingresa volumen 7:", value=" ")
+      
+  datos=pd.DataFrame({'Volumen': [a, b, c, d, e, f, g], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2), np.round(ph6, 2), np.round(ph7, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5", "Lab 6", "Lab 7"])
+  datos
+  st.line_chart(datos, x=[a, b, c, d, e, f, g], y='pH')
