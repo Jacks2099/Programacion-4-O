f34e18373af179d36e7403df997201128fbd5689
Jacks <116288286+Jacks2099@users.noreply.github.com>
2023-05-22 10:59:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ st.title('_Curvas de Titulación_')
 
 option = st.selectbox(
   'Selecciona el tipo de curva',
-  ('Acido-Base Fuerte', 'Acido Fuerte-Base Debil', 'Acido Debil-Base Fuerte'))
+  ('Acido-Base (Fuertes)', 'Base-Acido (Fuertes)', 'Experimental'))
 
 CA = st.text_input("Ingresa la concentración del _analito_:", value=" ")
 VA = st.text_input("Ingresa volumen del _analito_:", value=" ")
 CT = st.text_input("Ingresa la concentración del _titulante_:", value=" ")
 
-if option == 'Acido-Base Fuerte':
+if option == 'Acido-Base (Fuertes)':
   volumen = st.radio(
     '¿Cuantos volumenes requieres?',
     ('5', '6', '7', '8', '9', '10'))
@@ -21,34 +21,49 @@ if option == 'Acido-Base Fuerte':
       b= st.text_input("Ingresa volumen 2:", value=" ")
       c= st.text_input("Ingresa volumen 3:", value=" ")
       d= st.text_input("Ingresa volumen 4:", value=" ")
+      e= st.text_input("Ingresa volumen 5:", value=" ")
       
       mA = np.multiply(float(CA), float(VA))
       mT1 = np.multiply(float(CT), float(a))
+      if mT1<mA:
+        C1= (float(mA)-float(mT1))/(float(VA)+float(a))
+        ph1= np.log10(float(C1))*-1
+      elif mT1=mA:
+        ph1==7
+      elif mT1>mA:
+        C1= (float(mT1)-float(mA))/(float(VA)+float(a))
+        ph1= 14+np.log10(float(C1))
       mT2 = np.multiply(float(CT), float(b))
+      if mT2<mA:
+        C2= (float(mA)-float(mT1))/(float(VA)+float(b))
+        ph2= np.log10(float(C2))*-1
+      elif mT2=mA:
+        ph1==7
+      elif mT2>mA:
+        C2= (float(mT2)-float(mA))/(float(VA)+float(b))
+        ph2= 14+np.log10(float(C2))
       mT3 = np.multiply(float(CT), float(c))
+      if mT3<mA:
+        C3= (float(mA)-float(mT3))/(float(VA)+float(c))
+        ph3= np.log10(float(C3))*-1
+      elif mT3=mA:
+        ph1==7
+      elif mT3>mA:
+        C3= (float(mT3)-float(mA))/(float(VA)+float(c))
+        ph3= 14+np.log10(float(C3))
       mT4 = np.multiply(float(CT), float(d))
+      mTt = np.multiply(float(CT), float(e))
       
-      C1= (float(mA)-float(mT1))/(float(VA)+float(a))
-      C2= (float(mA)-float(mT2))/(float(VA)+float(b))
       C3= (float(mA)-float(mT3))/(float(VA)+float(c))
       C4= (float(mA)-float(mT4))/(float(VA)+float(d))
+      C4= (float(mA)-float(mT5))/(float(VA)+float(e))
       
-      ph1= np.log10(float(CA))*-1
-      ph2= np.log10(float(C1))*-1
-      ph3= np.log10(float(C2))*-1
-      ph4= np.log10(float(C3))*-1
-      ph5= np.log10(float(C4))*-1
+      ph3= np.log10(float(C3))*-1
+      ph4= np.log10(float(C4))*-1
+      ph5= np.log10(float(C5))*-1
       
-      datos=pd.DataFrame({'Volumen': [0, a, b, c, d], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
+      datos=pd.DataFrame({'Volumen': [a, b, c, d, e], 'pH':[np.round(ph1, 2), np.round(ph2, 2), np.round(ph3, 2), np.round(ph4, 2), np.round(ph5, 2)]}, index=["Lab 1", "Lab 2", "Lab 3", "Lab 4", "Lab 5"])
       datos
-      
-      st.header('Cálculos')
-      
-      st.write("pH=-log10("+str(CA)+")= "+str(np.round(ph1, 2)))
-      st.write("pH=-log10("+str(C1)+")= "+str(np.round(ph2, 2)))
-      st.write("pH=-log10("+str(C2)+")= "+str(np.round(ph3, 2)))
-      st.write("pH=-log10("+str(C3)+")= "+str(np.round(ph4, 2)))
-      st.write("pH=-log10("+str(C4)+")= "+str(np.round(ph5, 2)))
       
       st.line_chart(datos, x='Volumen', y='pH')
       
@@ -62,7 +77,7 @@ if option == 'Acido-Base Fuerte':
     st.write('En proceso:hammer:')
   elif volumen == '10':
     st.write('En proceso:hammer:')
-elif option == 'Acido Fuerte-Base Debil':
+elif option == 'Base-Acido (Fuertes)':
   st.write('En proceso:hammer:')
-elif option == 'Acido Debil-Base Fuerte':
+elif option == 'Experimental':
   st.write('En proceso:hammer:')
